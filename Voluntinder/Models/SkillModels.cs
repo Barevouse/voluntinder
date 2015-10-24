3760357ebce889ec7eb2abe9d9b5c0d58840de95
Bob Poole <Bob.poole@hronline.co.uk>
2015-10-24 15:50:06 +0100

    Few tweaks including  aiming db at azure

---
+++ Voluntinder/Models/SkillModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Voluntinder.Models
+{
+    public class SkillModels
+    {
+        public string UserId { get; set; }
+        public string Skill { get; set; }
+    }
+}
