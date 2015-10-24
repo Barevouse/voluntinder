79270a1dd82160823a562816db0362814218d1cd
Daniel Smith <daniel.smith@hronline.co.uk>
2015-10-24 22:52:35 +0100

    Added font-awesome, styled login buttons

--- Voluntinder/App_Start/BundleConfig.cs
+++ Voluntinder/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace Voluntinder
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
