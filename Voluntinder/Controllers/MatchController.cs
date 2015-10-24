a04d1965689e154584dfd62115fb6f547fc67ddb
Simon Chiu <si@sichiu.co.uk>
2015-10-24 16:44:10 +0100

    Added match controller

---
+++ Voluntinder/Controllers/MatchController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Voluntinder.Controllers
+{
+    public class MatchController : Controller
+    {
+        // GET: Match
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
