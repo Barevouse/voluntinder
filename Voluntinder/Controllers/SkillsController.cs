c813ee28ebc93ade2f71d7ed3092b2c0a2cae6e3
Bob Poole <Bob.poole@hronline.co.uk>
2015-10-24 16:21:48 +0100

    ALL THE THINGS entity

---
+++ Voluntinder/Controllers/SkillsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Voluntinder.Controllers
+{
+    public class SkillsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void SaveSkills(string userId, List<string> skills)
+        {
+            foreach (var skill in skills)
+            {
+                
+            }
+        }
+    }
+}
