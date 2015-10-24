8b3c7004d9bfd2972628e45f7be78815d8428898
Bob Poole <Bob.poole@hronline.co.uk>
2015-10-24 16:47:40 +0100

    adderd get skills

--- Voluntinder/Controllers/SkillsController.cs
+++ Voluntinder/Controllers/SkillsController.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ConsoleApplication1;
 
 namespace Voluntinder.Controllers
 {
     public class SkillsController : Controller
     {
+        public voluntinder_dbEntities Dbcontext;
         public ActionResult Index()
         {
             return View();
@@ -17,8 +20,15 @@ namespace Voluntinder.Controllers
         {
             foreach (var skill in skills)
             {
-                
+                Dbcontext.skills_list.Add(new skills_list {UserId = userId, Skills = skill});
             }
+            Dbcontext.SaveChanges();
+        }
+
+        public List<String> GetSkills(string userId)
+        {
+            var skills = Dbcontext.skills_list.Where(x => x.UserId == userId).Select(y=>y.Skills).ToList();
+            return skills;
         }
     }
 }
