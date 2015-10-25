1da5ddf563634d8d9aa4306875f2d912a3ad9475
Bob Poole <Bob.poole@hronline.co.uk>
2015-10-25 08:12:57 +0000

    began updating location and added a button when no matches appear to increase skills

--- Voluntinder/Models/MatchViewModel.cs
+++ Voluntinder/Models/MatchViewModel.cs
@@ -5,6 +5,7 @@ namespace Voluntinder.Models
     public class MatchViewModel
     {
         public string PageTitle { get; set; }
+        public string UserLocation { get; set; }
         public List<ProfileViewModel> Profile { get; set; }
     }
 }
